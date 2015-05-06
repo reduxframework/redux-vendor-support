a01106df79a1a694976e889857413f6b3d8599a7
KProvance <kevin.provance@gmail.com>
2015-05-05 22:16:19 -0400

    parse libs Signed-off-by:KProvance <kevin.provance@gmail.com>

--- inc/class.vendor-url.php
+++ inc/class.vendor-url.php
@@ -1,21 +1,22 @@
 <?php
 
-if ( ! class_exists( 'Redux_VendorURL' ) ) {
-    exit();
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
 }
 
 if (!class_exists('Redux_VendorURL')) {
     class Redux_VendorURL {
-        static public $url = '';
-        static public $dir = '';
+        static public $url;
+        static public $dir;
     
-        
-        public static function ace_editor() {
-            return self::$url . 'vendor/ace/ace.js';
-        }
-        
-        public static function select2() {
-            return self::$url . 'vendor/select2/select2.js';
+        public static function get_url($handle) {
+            if ($handle == 'ace-editor-js') {
+                return self::$url . 'vendor/ace/ace.js';
+            } elseif ($handle == 'select2-js') {
+                return self::$url . 'vendor/select2/select2.js';
+            } elseif ($handle == 'select2-css') {
+                return self::$url . 'vendor/select2/select2.css';
+            }
         }
     }
 }
