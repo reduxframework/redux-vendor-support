fee643261900d7dba38ac9062c900323e939c251
KProvance <kevin.provance@gmail.com>
2015-05-20 16:47:28 -0400

    remove qtip and spectrum Signed-off-by:KProvance <kevin.provance@gmail.com>

--- inc/class.vendor-url.php
+++ inc/class.vendor-url.php
@@ -16,14 +16,6 @@ if (!class_exists('Redux_VendorURL')) {
                 return self::$url . 'vendor/select2/select2.js';
             } elseif ($handle == 'select2-css') {
                 return self::$url . 'vendor/select2/select2.css';
-            } elseif ($handle == 'qtip-css') {
-                return self::$url . 'vendor/qtip/jquery.qtip.css';
-            } elseif ($handle == 'qtip-js') {
-                return self::$url . 'vendor/qtip/jquery.qtip.js';
-            } elseif ($handle == 'redux-spectrum-js') {
-                return self::$url . 'vendor/spectrum/redux-spectrum.js';
-            } elseif ($handle == 'redux-spectrum-css') {
-                return self::$url . 'vendor/spectrum/redux-spectrum.css';
             }
         }
     }
