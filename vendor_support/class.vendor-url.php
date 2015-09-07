709628ed030c02faa2de7cd86041f9ecbd621873
KProvance <kevin.provance@gmail.com>
2015-09-07 15:22:43 -0400

    ugh. Signed-off-by:KProvance <kevin.provance@gmail.com>

--- vendor_support/class.vendor-url.php
+++ vendor_support/class.vendor-url.php
@@ -1,22 +1,27 @@
 <?php
 
-    if ( ! defined( 'ABSPATH' ) ) {
-        exit;
-    }
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
 
-    if ( ! class_exists( 'Redux_VendorURL' ) ) {
-        class Redux_VendorURL {
-            static public $url;
-            static public $dir;
+if ( ! class_exists( 'Redux_VendorURL' ) ) {
+    class Redux_VendorURL {
+        static public $url;
+        static public $dir;
 
-            public static function get_url( $handle ) {
-                if ( $handle == 'ace-editor-js' && file_exists( self::$dir . 'vendor/ace_editor/ace' . Redux_Functions::isMin() . '.js' ) ) {
-                    return self::$url . 'vendor/ace_editor/ace.js';
-                } elseif ( $handle == 'select2-js' && file_exists( self::$dir . 'vendor/select2/select2' . Redux_Functions::isMin() . '.js' ) ) {
-                    return self::$url . 'vendor/select2/select2.js';
-                } elseif ( $handle == 'select2-css' && file_exists( self::$dir . 'vendor/select2/select2.css' )  ) {
-                    return self::$url . 'vendor/select2/select2.css';
-                }
+        public static function get_url( $handle ) {
+            $min    = Redux_Functions::isMin();
+            $ace    = self::$dir . 'vendor/ace_editor/ace.js';
+            $s2js   = self::$dir . 'vendor/select2/select2' . $min . '.js';
+            $s2css  = self::$dir . 'vendor/select2/select2.css';
+
+            if ( $handle == 'ace-editor-js' && file_exists( $ace ) ) {
+                return $ace;
+            } elseif ( $handle == 'select2-js' && file_exists( $s2js ) ) {
+                return $s2js;
+            } elseif ( $handle == 'select2-css' && file_exists( $s2css ) ) {
+                return $s2css;
             }
         }
     }
+}
