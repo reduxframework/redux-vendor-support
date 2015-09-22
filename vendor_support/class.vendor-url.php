18e08ff6086bb2c8caec12f5423dc1166e9cfa52
flxstn <flxstn@flxstn.com>
2015-09-22 12:57:21 +0200

    fix get_url() returns path instead of URL

--- vendor_support/class.vendor-url.php
+++ vendor_support/class.vendor-url.php
@@ -16,11 +16,11 @@ if ( ! class_exists( 'Redux_VendorURL' ) ) {
             $s2css  = self::$dir . 'vendor/select2/select2.css';
 
             if ( $handle == 'ace-editor-js' && file_exists( $ace ) ) {
-                return $ace;
+                return self::$url . 'vendor/ace_editor/ace.js';
             } elseif ( $handle == 'select2-js' && file_exists( $s2js ) ) {
-                return $s2js;
+                return self::$url . 'vendor/select2/select2.js';
             } elseif ( $handle == 'select2-css' && file_exists( $s2css ) ) {
-                return $s2css;
+                return self::$url . 'vendor/select2/select2.css';
             }
         }
     }
